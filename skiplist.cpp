b7cebbc9b575a75396e6803edd6437136bafe8ff
Yana Chala <ydc223@nyu.edu>
2018-02-18 21:08:21 +0400

    passing iss as

--- skiplist.cpp
+++ skiplist.cpp
@@ -430,7 +430,9 @@ void choose(string command, SkipList S, string arguments) {
 		        {
 			        getline(infile,line); // Saves the line in STRING.
 			        cout << line << endl;
-			        string command = line.substr(0, line.find(" "));
+			        istringstream iss(line);
+			        iss >> command;
+			        // string command = line.substr(0, line.find(" "));
 			        cout<<"Command: "<<command<<" Line: "<<line<<endl; // Prints our STRING.
 			        choose(command, S, line);
 		        }
